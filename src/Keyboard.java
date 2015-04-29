c2588835824798e667ba7c6b60671830b17a920f
erichall <hallstrom.eric@gmail.com>
2015-04-29 22:51:17 +0200

    FulFIske Försök till rörelse

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -6,6 +6,9 @@ import javafx.scene.shape.Rectangle;
 
 public class Keyboard implements EventHandler<KeyEvent> {
 	Rectangle r;
+	private int xMove = 1;
+	private int yMove = 1;
+	private int speed = 4;
 
 	public Keyboard(Rectangle r) {
 		this.r = r;
@@ -16,6 +19,7 @@ public class Keyboard implements EventHandler<KeyEvent> {
 		if (event.getCode() == KeyCode.W) {
 			
 		}
+		
 		System.out.println("You pressed the "+event.getCode()+" key!");
 		
 		final long startTime = System.nanoTime();
@@ -23,7 +27,50 @@ public class Keyboard implements EventHandler<KeyEvent> {
 			@Override
 			public void handle(long now) {	
 				//r.setX(r.getX()+1);
-				r.setRotate(r.getRotate()+.5);
+				//r.setRotate(r.getRotate()+.5);
+				if(r.getLayoutX() > 400 || r.getLayoutX() < 0){
+					xMove =xMove*-1;
+					speed--;
+				}else if(r.getLayoutY() > 400 || r.getLayoutY() < 0){
+					yMove = yMove*-1;
+					speed--;
+				}
+				
+				switch(event.getCode()){
+				case A:
+					if(xMove > 0){
+					xMove = xMove*-1;
+					r.setLayoutX(xMove+speed);
+					}else{
+						r.setLayoutX(xMove +speed);
+					}
+				case D:
+					if(xMove < 0){
+						xMove = xMove*-1;
+						r.setLayoutX(xMove+speed);
+					}else{
+						r.setLayoutX(xMove+speed);
+					}
+				
+				case W:
+					if(yMove < 0){
+						yMove = yMove*-1;
+						r.setLayoutY(yMove);
+					}else{
+						r.setLayoutY(yMove);
+					}
+				case S:
+					if(yMove > 0){
+						yMove = yMove*-1;
+						r.setLayoutY(yMove);
+					}else{
+						r.setLayoutY(yMove);
+					}
+					default:
+						speed++;
+				}
+				
+				System.out.println("XMOVE: " + xMove + " YMOVE: " + yMove);
 				if(now > startTime+Math.pow(10, 9)*10)
 					this.stop();
 			}

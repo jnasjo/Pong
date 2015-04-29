dcdd7e25362e5a0744cdde7a5ae730b90f7a7f5c
erichall <hallstrom.eric@gmail.com>
2015-04-29 23:24:03 +0200

    nudå

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -17,13 +17,13 @@ public class Keyboard implements EventHandler<KeyEvent> {
 
 	@Override
 	public void handle(KeyEvent event) {
-<<<<<<< HEAD
 	
 		
 		System.out.println("You pressed the "+event.getCode()+" key!");
 		
 		
-		
+		yMove += speedY;
+	
 		
 		switch(event.getCode()){
 		case A:
@@ -60,7 +60,6 @@ public class Keyboard implements EventHandler<KeyEvent> {
 				speedY = -speedY;
 				r.setLayoutY(yMove);
 			}else{
-				yMove -=speedY;
 				r.setLayoutY(yMove);
 			}
 			default:
@@ -74,64 +73,6 @@ public class Keyboard implements EventHandler<KeyEvent> {
 								
 				
 				if(now > startTime+Math.pow(10, 9)*10)
-=======
-		if (event.getCode() == KeyCode.W) {
-
-		}
-
-		System.out.println("You pressed the " + event.getCode() + " key!");
-
-		final long startTime = System.nanoTime();
-		final AnimationTimer a = new AnimationTimer() {
-			@Override
-			public void handle(long now) {
-				// r.setX(r.getX()+1);
-				// r.setRotate(r.getRotate()+.5);
-				if (r.getLayoutX() > 400 || r.getLayoutX() < 0) {
-					xMove = xMove * -1;
-					speed--;
-				} else if (r.getLayoutY() > 400 || r.getLayoutY() < 0) {
-					yMove = yMove * -1;
-					speed--;
-				}
-
-				switch (event.getCode()) {
-				case A:
-					if (xMove > 0) {
-						xMove = xMove * -1;
-						r.setLayoutX(xMove + speed);
-					} else {
-						r.setLayoutX(xMove + speed);
-					}
-				case D:
-					if (xMove < 0) {
-						xMove = xMove * -1;
-						r.setLayoutX(xMove + speed);
-					} else {
-						r.setLayoutX(xMove + speed);
-					}
-
-				case W:
-					if (yMove < 0) {
-						yMove = yMove * -1;
-						r.setLayoutY(yMove);
-					} else {
-						r.setLayoutY(yMove);
-					}
-				case S:
-					if (yMove > 0) {
-						yMove = yMove * -1;
-						r.setLayoutY(yMove);
-					} else {
-						r.setLayoutY(yMove);
-					}
-				default:
-					speed++;
-				}
-
-				System.out.println("XMOVE: " + xMove + " YMOVE: " + yMove);
-				if (now > startTime + Math.pow(10, 9) * 10)
->>>>>>> 619cf7fe8eab2986521baf54c33c6b5c915eb569
 					this.stop();
 			}
 		};

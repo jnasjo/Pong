aaf154bedb2c6007585d7dc29a45bcde96865539
erichall <hallstrom.eric@gmail.com>
2015-04-29 23:21:09 +0200

    WE CAN MOVE

--- src/Keyboard.java
+++ src/Keyboard.java
@@ -8,7 +8,8 @@ public class Keyboard implements EventHandler<KeyEvent> {
 	Rectangle r;
 	private int xMove = 1;
 	private int yMove = 1;
-	private int speed = 4;
+	private int speedX = 4;
+	private int speedY = 4;
 
 	public Keyboard(Rectangle r) {
 		this.r = r;
@@ -16,61 +17,61 @@ public class Keyboard implements EventHandler<KeyEvent> {
 
 	@Override
 	public void handle(KeyEvent event) {
-		if (event.getCode() == KeyCode.W) {
-			
-		}
+	
 		
 		System.out.println("You pressed the "+event.getCode()+" key!");
 		
+		
+		
+		
+		switch(event.getCode()){
+		case A:
+			if(speedX > 0){
+				xMove -= speedX;
+				speedX = -speedX;
+				r.setLayoutX(xMove);
+			}else{
+				xMove -= speedX;
+				r.setLayoutX(xMove);
+			}
+		case D:
+			if(speedX < 0){
+				xMove += speedX;
+				speedX = -speedX;
+				r.setLayoutX(xMove);
+			}else{
+				xMove += speedX;
+				r.setLayoutX(xMove);
+			}
+		
+		case W:
+			if(speedY < 0){
+				yMove += speedY;
+				speedY = -speedY;
+				r.setLayoutY(yMove);
+			}else{
+				yMove += speedY;
+				r.setLayoutY(yMove);
+			}
+		case S:
+			if(speedY > 0){
+				yMove -= speedY;
+				speedY = -speedY;
+				r.setLayoutY(yMove);
+			}else{
+				yMove -=speedY;
+				r.setLayoutY(yMove);
+			}
+			default:
+				
+		}
+		System.out.println("XMOVE: " + xMove + " YMOVE: " + yMove);
 		final long startTime = System.nanoTime();
 		final AnimationTimer a = new AnimationTimer() {
 			@Override
 			public void handle(long now) {	
-				//r.setX(r.getX()+1);
-				//r.setRotate(r.getRotate()+.5);
-				if(r.getLayoutX() > 400 || r.getLayoutX() < 0){
-					xMove =xMove*-1;
-					speed--;
-				}else if(r.getLayoutY() > 400 || r.getLayoutY() < 0){
-					yMove = yMove*-1;
-					speed--;
-				}
+								
 				
-				switch(event.getCode()){
-				case A:
-					if(xMove > 0){
-					xMove = xMove*-1;
-					r.setLayoutX(xMove+speed);
-					}else{
-						r.setLayoutX(xMove +speed);
-					}
-				case D:
-					if(xMove < 0){
-						xMove = xMove*-1;
-						r.setLayoutX(xMove+speed);
-					}else{
-						r.setLayoutX(xMove+speed);
-					}
-				
-				case W:
-					if(yMove < 0){
-						yMove = yMove*-1;
-						r.setLayoutY(yMove);
-					}else{
-						r.setLayoutY(yMove);
-					}
-				case S:
-					if(yMove > 0){
-						yMove = yMove*-1;
-						r.setLayoutY(yMove);
-					}else{
-						r.setLayoutY(yMove);
-					}
-					default:
-						speed++;
-				}
-				
-				System.out.println("XMOVE: " + xMove + " YMOVE: " + yMove);
 				if(now > startTime+Math.pow(10, 9)*10)
 					this.stop();
 			}
